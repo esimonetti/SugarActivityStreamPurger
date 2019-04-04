3505f123b84cf0ab2270f601422fc3095792e359
Enrico Simonetti <enrico.simonetti@gmail.com>
2019-04-05 10:31:04 +1100

    Added v9

--- configuration/manifest.php
+++ configuration/manifest.php
@@ -4,4 +4,4 @@ $manifest['built_in_version'] = '8.0.0';
 $manifest['name'] = 'CLI command and Scheduler job to purge from database old Activity Stream records';
 $manifest['description'] = 'SugarActivityStreamPurger is a Sugar installable package containing both a CLI command and a Scheduler job to purge from database old Activity Stream records';
 $manifest['author'] = 'Enrico Simonetti';
-$manifest['acceptable_sugar_versions']['regex_matches'] = array('^8.[\d]+.[\d]+$');
+$manifest['acceptable_sugar_versions']['regex_matches'] = array('^8.[\d]+.[\d]+$', '^9.0.[\d]+$');
